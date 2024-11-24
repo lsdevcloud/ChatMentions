e9a43c1fcb9a09936523ad1f134c1cc0ee1458cd
lsdevcloud <info@lsdevcloud.com>
2024-11-24 16:47:31 +0100

    Initial commit

---
+++ src/main/java/com/lsdevcloud/chatmentions/ChatMentions.java
@@ -0,0 +1,17 @@
+package com.lsdevcloud.chatmentions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChatMentions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
